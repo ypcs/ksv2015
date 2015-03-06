57f4c3276bdfe0626098f0f39f10e8b186623093
Ville Korhonen <ville@xd.fi>
2015-03-06 17:45:04 +0200

    Add new image size.

--- functions.php
+++ functions.php
@@ -2,5 +2,7 @@
 
 load_theme_textdomain('ksv2015', get_stylesheet_directory() . '/languages');
 
+add_image_size('medium-ksv', 800, 600, false);
+
 ini_set('upload_max_filesize', '10M');
 ini_set('post_max_size', '10M');
